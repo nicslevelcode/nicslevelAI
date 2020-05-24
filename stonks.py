e9b79c96cf8b0798146c4924f345029abc49425f
neckislong <neckislong@users.noreply.github.com>
2020-05-24 23:00:08 +0800

    Update stonks.py

--- stonks.py
+++ stonks.py
@@ -31,8 +31,51 @@ for n in [14, 30, 50, 200]:
                               timeperiod=n) / lng_df['Adj_Close']
     # Create the RSI indicator
     lng_df['rsi' + str(n)] = talib.RSI(lng_df['Adj_Close'].values, timeperiod=n)
-    
     # Add rsi and moving average to the feature name list
     feature_names = feature_names + ['ma' + str(n), 'rsi' + str(n)]
-   
 print(feature_names)
+
+# Drop all na values
+lng_df = lng_df.dropna()
+# Create features and targets
+# use feature_names for features; '5d_close_future_pct' for targets
+features = lng_df[feature_names]
+targets = lng_df['5d_close_future_pct']
+# Create DataFrame from target column and feature columns
+feature_and_target_cols = ['5d_close_future_pct'] + feature_names
+feat_targ_df = lng_df[feature_and_target_cols]
+# Calculate correlation matrix
+corr = feat_targ_df.corr()
+print(corr)
+# Plot heatmap of correlation matrix
+sns.heatmap(corr, annot=True, annot_kws = {"size": 14})
+plt.yticks(rotation=0, size = 14); plt.xticks(rotation=90, size = 14)  # fix ticklabel directions and size
+plt.tight_layout()  # fits plot area to the plot, "tightly"
+plt.show()  # show the plot
+plt.clf()  # clear the plot area
+# Create a scatter plot of the most highly correlated variable with the target
+plt.scatter(lng_df['ma200'], lng_df['5d_close_future_pct'])
+plt.show()
+
+# Import the statsmodels.api library with the alias sm
+import statsmodels.api as sm
+# Add a constant to the features
+linear_features = sm.add_constant(features)
+# Create a size for the training set that is 85% of the total number of samples
+train_size = int(0.85 * features.shape[0])
+train_features = linear_features[:train_size]
+train_targets = targets[:train_size]
+test_features = linear_features[train_size:]
+test_targets = targets[train_size:]
+print(linear_features.shape, train_features.shape, test_features.shape)
+
+# Create the linear model and complete the least squares fit
+model = sm.OLS(train_targets, train_features)
+results = model.fit()  # fit the model
+print(results.summary())
+# examine pvalues
+# Features with p <= 0.05 are typically considered significantly different from 0
+print(results.pvalues)
+# Make predictions from our model for train and test sets
+train_predictions = results.predict(train_features)
+test_predictions = results.predict(test_features)
